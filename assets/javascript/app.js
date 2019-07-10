5533eeb1f400580aa658945b66daab4c84655efb
Juthika Shetye <juthika.bhonsle@gmail.com>
2019-07-10 14:35:59 -0700

    added links,images in guide

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,7 +27,30 @@ $(document).ready(function(){
   		$("#people").val("");
   	}
 
-  })
+  });
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 	
 });
 
@@ -155,20 +178,11 @@ function splitLogic(){
 			}
 		}
 
-		
 	})
-
 
 }
 
 function scrollToResult() {
-  // window.scrollTo(0, 500);
-
-//   window.scroll({
-//   top: 650, 
-//   left: 0, 
-//   behavior: 'smooth'
-// });
 
 	document.querySelector('#totalSection').scrollIntoView({ 
 	  behavior: 'smooth' 
